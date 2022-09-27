da0b19c5543c9ff35052a7bfd28763fbc1936a17
Ashimasadana <42927268+Ashimasadana@users.noreply.github.com>
2022-09-26 22:57:02 -0400

    Delete retispec/RetiSpec directory

--- retispec/RetiSpec/acquisition/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcquisitionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'acquisition'
--- retispec/RetiSpec/acquisition/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AddAcquisition, ListAcquistionsOfPatient, delete_view, DownloadAcquistionImage
-
-urlpatterns = [
-               path('AddAcquisition/', AddAcquisition.as_view(), name='AddAcquisition'),
-               path('ListAcquistionsOfPatient/<id>/', ListAcquistionsOfPatient.as_view(), name='ListAcquistionsOfPatient'),
-               path('DeleteAcquisition/<id>/',delete_view,name='DeleteAcquisition'),
-               path('DownloadAcquistionImage/<id>/', DownloadAcquistionImage, name='DownloadAcquistionImage'),
-]
